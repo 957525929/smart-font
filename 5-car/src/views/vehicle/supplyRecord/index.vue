d6ad41d80fec052c53600afa616987f6dca0faa1
张家锐 <374358121@qq.com>
2021-07-02 16:43:13 +0800

    保养记录

--- 5-car/src/views/vehicle/supplyRecord/index.vue
+++ 5-car/src/views/vehicle/supplyRecord/index.vue
@@ -9,7 +9,7 @@
           <a-select-option key="3" value="司机">司机</a-select-option>
         </a-select>
         <a-input-search :placeholder="`请输入${currentCondition}搜索`" style="width: 200px" @search="onSearch" />
-        <a-button type="primary" style="float:right;margin-right:10px">导出</a-button>
+        <a-button type="primary" style="float: right; margin-right: 10px">导出</a-button>
       </div>
     </div>
 
@@ -21,43 +21,45 @@
         <a slot="img">查看</a>
       </a-table>
     </div>
-    
-
   </div>
 </template>
 
 <script>
-const columns=[
+const columns = [
+  {
+    title: '车牌号',
+    dataIndex: 'licenseNum',
+  },
   {
     title: '加油时间',
-    dataIndex: 'supplyTime'
+    dataIndex: 'supplyTime',
   },
   {
     title: '加油类型',
-    dataIndex: 'supplyType'
+    dataIndex: 'supplyType',
   },
   {
     title: '加油站',
-    dataIndex: 'gasStation'
+    dataIndex: 'gasStation',
   },
-   {
+  {
     title: '金额',
-    dataIndex: 'amount'
+    dataIndex: 'amount',
   },
-   {
+  {
     title: '油料单价',
-    dataIndex: 'unitPrice'
+    dataIndex: 'unitPrice',
   },
   {
     title: '加油时公里数',
-    dataIndex: 'distance'
+    dataIndex: 'distance',
   },
-   {
+  {
     title: '油卡',
     dataIndex: 'card',
     scopedSlots: { customRender: 'card' },
   },
-   {
+  {
     title: '报销司机',
     dataIndex: 'driver',
     scopedSlots: { customRender: 'driver' },
@@ -71,26 +73,27 @@ const columns=[
     title: '票据图片',
     dataIndex: 'img',
     scopedSlots: { customRender: 'img' },
-  }
+  },
 ]
 const tableData = [
   {
-    supplyTime:'2021-06-29 9:35',
-    supplyType:'汽油#92',
-    gasStation:'上三路加油站',
-    amount:'88元',
-    unitPrice:'8.01元/升',
-    distance:'4.6',
-    driver:'张三',
-    record:'测A123404-2021-06-29-001'
-  }
+    licenseNum: '测A123401',
+    supplyTime: '2021-06-29 9:35',
+    supplyType: '汽油#92',
+    gasStation: '上三路加油站',
+    amount: '88元',
+    unitPrice: '8.01元/升',
+    distance: '4.6',
+    driver: '张三',
+    record: '测A123404-2021-06-29-001',
+  },
 ]
 export default {
   data() {
     return {
       tableData: tableData,
-      columns:columns,
-      currentCondition: '车牌号'
+      columns: columns,
+      currentCondition: '车牌号',
     }
   },
   methods: {
@@ -128,5 +131,4 @@ export default {
     padding: 20px;
   }
 }
-
 </style>
