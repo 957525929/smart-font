22427786e4919b94409ccb8e22c425f4205dd452
Jiang <957525929@qq.com>
2021-07-08 17:12:27 +0800

    增加文档类型管理

---
+++ 8-synthesize/src/views/systemSetup/js/DocType.js
@@ -0,0 +1,60 @@
+export const data = [
+    {
+        key: '1',
+        docType: '资产',
+        createTime: '2020-5-23'
+    },
+    {
+        key: '2',
+        docType: '会议记要',
+        createTime: '2020-5-23'
+    },
+    {
+        key: '3',
+        docType: '记录',
+        createTime: '2020-6-21'
+    },
+    {
+        key: '4',
+        docType: '总结',
+        createTime: '2020-2-28'
+    },
+
+]
+export const columns = [
+    {
+        title: '类型编号',
+        dataIndex: '',
+        key: 'rowIndex',
+        width: 90,
+        align: "center",
+        customRender: function (t, r, index) {
+            return parseInt(index) + 1;
+        }
+    },
+    {
+        title: '文件类型',
+        align: "center",
+        dataIndex: 'docType',
+        width: 200,
+        scopedSlots: { customRender: 'docTypeList' }
+    },
+    {
+        title: '创建时间',
+        align: "center",
+        dataIndex: 'createTime',
+        width: 200,
+        unhidden: true
+        /*            customRender:function (text) {
+                      return "*"+text.substring(9,text.length);
+                    }*/
+    },
+    {
+        title: '操作',
+        dataIndex: 'action',
+        align: "center",
+        width: 180,
+        scopedSlots: { customRender: 'action' },
+
+    }
+]
--- 8-synthesize/src/views/systemSetup/modules/DocModal.vue
+++ 8-synthesize/src/views/systemSetup/modules/DocModal.vue
@@ -1,32 +1,13 @@
 <template>
-  <a-modal :title="title" :width="800" :visible="visible" :confirmLoading="confirmLoading" @ok="handleOk" @cancel="handleCancel" okText="保存任务" cancelText="关闭">
+  <a-modal :title="title" :width="800" :visible="visible" :confirmLoading="confirmLoading" @ok="handleOk" @cancel="handleCancel" okText="保存文件" cancelText="关闭">
 
     <a-spin :spinning="confirmLoading">
       <a-form :form="form">
 
-        <a-form-item :labelCol="labelCol" :wrapperCol="wrapperCol" label="任务名称" hasFeedback>
-          <a-input style="width:200px" placeholder="请输入任务名称" v-decorator="['taskName', {rules: [{ required: true, message: '请输入任务名称!' }]}]" />
+        <a-form-item :labelCol="labelCol" :wrapperCol="wrapperCol" label="文件名称" hasFeedback>
+          <a-input style="width:200px" placeholder="请输入文件名称" v-decorator="['docName', {rules: [{ required: true, message: '请输入任务名称!' }]}]" />
         </a-form-item>
-        <a-form-item :labelCol="labelCol" :wrapperCol="wrapperCol" label="重要性" hasFeedback priority>
-          <a-select style="width:130px" placeholder="请选择重要性" ref="select" v-decorator="['priority', {rules: [{ required: true, message: '请选择重要性!' }]}]">
-            <a-select-option value="0">高</a-select-option>
-            <a-select-option value="1">中</a-select-option>
-            <a-select-option value="2">低</a-select-option>
-          </a-select>
-        </a-form-item>
-        <a-form-item v-if="this.model.key" :labelCol="labelCol" :wrapperCol="wrapperCol" label="任务状态">
-          <a-select style="width:100px" ref="select" v-decorator="['status']">
-            <a-select-option value="0">进行中</a-select-option>
-            <a-select-option value="1">未开始</a-select-option>
-            <a-select-option value="2">已完成</a-select-option>
-            <a-select-option value="3">未完成</a-select-option>
-          </a-select>
-        </a-form-item>
-        <a-form-item :labelCol="labelCol" :wrapperCol="wrapperCol" label="时间">
-          <a-date-picker class="w140" placeholder="开始时间" v-decorator="['startTime', {rules: [{ required: true, message: '请选择开始时间!' }]}]" />
-          ~
-          <a-date-picker class="w140" placeholder="结束时间" v-decorator="['deadline', {rules: [{ required: true, message: '请选择结束时间!' }]}]" />
-        </a-form-item>
+
         <a-form-item :labelCol="labelCol" :wrapperCol="wrapperCol" label="上传文件">
           <a-upload name="file" :multiple="true" :headers="headers" @change="handleChange">
             <a-input placeholder="请上传文件" v-decorator="[ 'document', validatorRules.document]">
@@ -46,7 +27,7 @@
 import { httpAction } from '@/api/manage'
 import JCron from "@/components/jeecg/JCron";
 import pick from 'lodash.pick'
-import moment from "moment"
+// import moment from "moment"
 
 export default {
   name: "TaskListModel",
@@ -94,11 +75,10 @@ export default {
       this.model = Object.assign({}, record);
       console.log(this.model)
       this.visible = true;
-      this.model.startTime = moment(this.model.startTime, 'YYYY-MM-DD');
-      this.model.deadline = moment(this.model.deadline, 'YYYY-MM-DD');
       this.$nextTick(() => {
-        this.form.setFieldsValue(pick(this.model, 'taskName', 'priority', 'status', 'deadline', 'startTime'));
+        this.form.setFieldsValue(pick(this.model, 'docName', 'priority', 'status', 'deadline', 'startTime'));
       });
+
     },
     close() {
       this.$emit('close');
@@ -110,6 +90,10 @@ export default {
       this.form.validateFields((err, values) => {
         console.log('values', values)
         if (!err) {
+          if (typeof values.cronExpression == "undefined" || Object.keys(values.cronExpression).length == 0) {
+            this.$message.warning('请输入cron表达式!');
+            return false;
+          }
 
           that.confirmLoading = true;
           let httpurl = '';
