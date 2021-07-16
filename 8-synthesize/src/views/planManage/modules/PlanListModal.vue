34fabdb47707942cb067e0c67684330fe93bf7ec
‘jiang’ <957525929@qq.com>
2021-07-16 09:43:47 +0800

    超时状态，提醒周期的修改

--- 8-synthesize/src/views/planManage/modules/PlanListModal.vue
+++ 8-synthesize/src/views/planManage/modules/PlanListModal.vue
@@ -23,8 +23,7 @@
             <a-select-option value="0">未开始</a-select-option>
             <a-select-option value="1">进行中</a-select-option>
             <a-select-option value="2">已完成</a-select-option>
-            <a-select-option value="3">未完成</a-select-option>
-            <a-select-option value="4">延期中</a-select-option>
+            <a-select-option value="3">已超时</a-select-option>
           </a-select>
         </a-form-item>
         <a-form-item :labelCol="labelCol" :wrapperCol="wrapperCol" label="时间">
