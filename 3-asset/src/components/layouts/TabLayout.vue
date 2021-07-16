d34da57d18cf7e87e8632d4be4e34c4e12a8bbb4
lyra <675935445@qq.com>
2021-07-16 16:39:02 +0800

    标题修改

--- 3-asset/src/components/layouts/TabLayout.vue
+++ 3-asset/src/components/layouts/TabLayout.vue
@@ -165,7 +165,7 @@
 
       // update-begin-author:sunjianlei date:20200120 for: 动态更改页面标题
       changeTitle(title) {
-        let projectTitle = "烟草公司"
+        let projectTitle = "资产管理系统"
         // 首页特殊处理
         if (this.$route.path === indexKey) {
           document.title = projectTitle
