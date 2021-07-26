19a4fdc14c0fe0e9df4be5928577d72da3106808
lyra <675935445@qq.com>
2021-07-26 15:53:58 +0800

    07APPv-for

--- 7-app/pages/record/timelineHotel.vue
+++ 7-app/pages/record/timelineHotel.vue
@@ -13,65 +13,38 @@
 				</picker>
 			</view>
 		</view>
-		<view class="cu-timeline">
-			
-			<view class="cu-item text-blue" @click="modalVisable1 = true">
+		<view class="cu-timeline" v-for="(item,index) in hoteldate" >
+			<view class="cu-item text-blue" @click="item.modalVisable = true">
 				<view class="cu-list menu-avatar radius" >
 					<view class="cu-item bg-gradual-blue">
 						<view class="content" style="left: 8px;">
-							<text>2021-06-05</text> 
-							<view class="margin-top">福州富力威斯汀酒店酒店预定</view>
+							<text>{{item.time}}</text> 
+							<view class="margin-top">{{item.name}}</view>
 						</view>
 					</view>
 				</view>
 			</view>
-			
-			<view class="cu-item text-blue" @click="modalVisable2 = true">
-				<view class="cu-list menu-avatar radius" >
-					<view class="cu-item  bg-gradual-blue">
-						<view class="content" style="left: 8px;">
-							<text>2021-06-06</text> 
-							<view class="margin-top">福州品悦酒店酒店预定</view>
-						</view>
-					</view>
-				</view>
-			</view>
-
-			<view class="cu-item text-blue" @click="modalVisable3 = true">
-				<view class="cu-list menu-avatar radius" >
-					<view class="cu-item  bg-gradual-blue">
-						<view class="content" style="left: 8px;">
-							<text>2021-06-07</text> 
-							<view class="margin-top">福州世纪金源酒店酒店预定</view>
-						</view>
-					</view>
-				</view>
-			</view>
-			<view class="cu-modal" :class="modalVisable1==true?'show':''">
+			<view v-for="(item,index) in hoteldate">
+			<view class="cu-modal" :class="item.modalVisable==true?'show':''">
 				<view class="cu-dialog" style="height: 30%;">
 					<view class="cu-bar bg-white justify-end">
 						<view class="content">预订详情</view>
-						<view class="action" @tap="modalVisable1 = false">
+						<view class="action" @tap="item.modalVisable = false">
 							<text class="cuIcon-close text-red"></text>
 						</view>
 					</view>
 					<view class="padding-xl" style="padding: 0px;height:100%;">
 						<view class="cu-form-group align-start"  style="height:65%;">
-							<textarea maxlength="-1" disabled=true @input="textareaBInput"  style="height:100%;text-align:left;"
-							placeholder="预定酒店：福州富力威斯汀酒店
-											预约时间：2021-06-5号至8号(共3天)
-											酒店地址:福州江滨中大道366号
-											联系人:李霞   电话号码:13759655332
-											备注:两间标间,入住人:刘嘉林,李查,林夕,郑华
-											"
-							>
+							<textarea maxlength="-1" disabled=true @input="textareaBInput"  style="height:100%;text-align:left;">
+							{{item.detail}}
 							</textarea>
 							</br>
 						</view>
 					</view>
 				</view>
 			</view>
-			<view class="cu-modal" :class="modalVisable2==true?'show':''">
+			</view>
+			<!-- <view class="cu-modal" :class="modalVisable2==true?'show':''">
 				<view class="cu-dialog" style="height: 30%;">
 					<view class="cu-bar bg-white justify-end">
 						<view class="content">预订详情</view>
@@ -118,7 +91,7 @@
 						</view>
 					</view>
 				</view>
-			</view>
+			</view> -->
 			
 		</view>
 	</view>
@@ -131,10 +104,31 @@
 				indexDate:0,
 				date: "2021-06",
 				size: false,
-				modalVisable: false,
-				modalVisable1: false,
-				modalVisable2: false,
-				modalVisable3: false,
+				k:"",
+				// modalVisable: false,
+				// modalVisable1: false,
+				// modalVisable2: false,
+				// modalVisable3: false,
+				hoteldate:[
+					{
+					modalVisable:false,
+					time:'2021-06-05',
+					name:'福州富力威斯汀酒店预定',
+					detail:"预定酒店：福州富力威斯汀酒店  预约时间：2021-06-05号至08号(共3天)	酒店地址:福州东浦路59号	联系人:李霞   电话号码:13759655332 备注:两间标间,入住人:刘嘉林,李查,林夕,郑华"
+					},
+					{
+					modalVisable:false,
+					time:'2021-06-06',
+					name:'福州品悦酒店',
+					detail:"预定酒店：福州品悦酒店  预约时间：2021-06-06号至10号(共4天)	酒店地址:福州东浦路59号	联系人:尤晓梅   电话号码:13053955537 备注:一间标间,入住人:刘毅,吴查"
+					},
+					{
+					modalVisable:false,
+					time:'2021-06-07',
+					name:'福州世纪金源酒店预定',
+					detail:"预定酒店：福州世纪金源酒店  预约时间：2021-06-07号至09号(共2天)	酒店地址:福州温泉公园路59号	联系人:尤晓梅   电话号码:13053955537 备注:一间标间,入住人:刘毅,吴查"
+					},
+				]
 			};
 		},
 		methods:{
