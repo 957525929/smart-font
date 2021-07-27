8b7ccce77cb6350cded0b97359904bfdc66cd29f
孙颖君 <576793046@qq.com>
2021-07-27 19:07:52 +0800

    数据修改

--- 6-app/pages/workplace/protectTask/protectTaskDetail.vue
+++ 6-app/pages/workplace/protectTask/protectTaskDetail.vue
@@ -12,8 +12,11 @@
 				</view>
 			</view>
 		</scroll-view>
-
-		<component :is="comType" :dataSource="sourceData" :picList="img" :item="item" v-for="item in sourceData" :key="item.devId">
+		
+		<DescriptionDetail :dataSource="sourceData" :picList="img" v-if="TabCur===0"></DescriptionDetail>
+		<TaskCardItem :item="item" v-for="item in sourceData" :key="item.devId" v-else-if="TabCur===1"></TaskCardItem>
+		<DescriptionDetail :dataSource="sourceData"  :picList="img" v-else-if="TabCur===2"></DescriptionDetail>
+		<FormDetail :dataSource="sourceData" v-else-if="TabCur===3">
 			<textarea slot="reason" placeholder="请输入" class="bg-grey radius margin"></textarea>
 			<textarea slot="handle" placeholder="请输入" class="bg-grey radius margin"></textarea>
 			<picker mode="time" :value="time" :start="time" @change="TimeChange"
@@ -28,7 +31,7 @@
 					{{date}}
 				</view>
 			</picker>
-		</component>
+		</FormDetail>
 	</view>
 </template>
 
@@ -54,43 +57,37 @@
 			this.date = getNow()[0].join("-")
 			this.time = getNow()[1].join(":")
 			//拿数据
-			this.getData('proAllBasicData', 'proBasic')
+			this.getData()
+			this.getDev()
 		},
 		components: {
 			DescriptionDetail,
 			FormDetail
-		},
-		computed: {
-			comType() {
-				return this.menuList[this.TabCur].comType
-			}
 		},
 		data() {
 			return {
 				img: NEW_PRODETAIL.img,
 				TabCur: 0,
 				taskId: "",
-				sourceData: [],
+				areaKey:"",
+				basicData: [],
+				devData:[],
 				menuList: [{
 					label: "proAllBasicData",
 					value: "基本信息",
-					dataIndex: "proBasic",
-					comType: "DescriptionDetail"
+					dataIndex: "proBasic"
 				},{
 					label: "devData",
 					value: "养护设备",
-					dataIndex: "proDev",
-					comType: "TaskCardItem"
+					dataIndex: "proDev"
 				},{
 					label: "proAllPlanData",
 					value: "养护记录",
-					dataIndex: "proPlan",
-					comType: "DescriptionDetail"
+					dataIndex: "proPlan"
 				},  {
 					label: "proForm",
 					value: "表单上报",
-					dataIndex: "proForm",
-					comType: "FormDetail"
+					dataIndex: "proForm"
 				}],
 				//time-picker
 				date: "",
@@ -102,11 +99,42 @@
 				this.TabCur = e.currentTarget.dataset.id;
 				this.getData(this.menuList[this.TabCur].label, this.menuList[this.TabCur].dataIndex)
 			},
+			getBasic(){
+				this.basicData = []
+				let tempData = NEW_PRODETAIL.proAllBasicData.filter(item =>{
+					return item.taskId == this.taskId
+				})[0]
+				this.basicData = NEW_PRODETAIL.proBasic.filter(i=>!i.hideInDetail).map(item => {
+					if (item.valueEnum&&item.valueEnum.length!==0) {
+					    item.value = handleEnumData(item.valueEnum,tempData[item.key])
+					}else{
+						item.value = tempData[item.key]
+					}
+					if(item.key==="areaKey"){
+						this.areaKey = tempData[item.key]
+					}
+					return item
+				})
+			},
+			getDev(){
+				this.devData = []
+				let tempData = NEW_PRODETAIL.devData.filter(item =>{
+					return item.areaKey == this.taskId
+				})[0]
+				this.devData = NEW_PRODETAIL.proDev.filter(i=>!i.hideInDetail).map(item => {
+					if (item.valueEnum&&item.valueEnum.length!==0) {
+					    item.value = handleEnumData(item.valueEnum,tempData[item.key])
+					}else{
+						item.value = tempData[item.key]
+					}
+					return item
+				})
+			},
 			getData(name, dataIndex) {
 				this.sourceData = []
 				if (name !== "proForm") {
 					let tempData = NEW_PRODETAIL[name].filter(item =>{
-						return item.taskId == this.taskId
+						return item.taskId == this.areaKey
 					})[0]
 					this.sourceData = NEW_PRODETAIL[dataIndex].filter(i=>!i.hideInDetail).map(item => {
 						if (item.valueEnum&&item.valueEnum.length!==0) {
