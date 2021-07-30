1f1ce268e5f864583e2cb0ab01a68cff79a7d420
wangxiaofeng <287386601@qq.com>
2021-07-30 14:59:12 +0800

    06物业系统 build设备 、修改显示标题、修改logo

--- 6-property/src/utils/util.js
+++ 6-property/src/utils/util.js
@@ -11,7 +11,8 @@ export function timeFix() {
 }
 
 export function welcome() {
-    const arr = ['休息一会儿吧', '准备吃什么呢?', '要不要打一把 DOTA', '我猜你可能累了']
+    //const arr = ['休息一会儿吧', '准备吃什么呢?', '要不要打一把 DOTA', '我猜你可能累了']
+  const arr = ['欢迎进入物业管理系统.']
     let index = Math.floor((Math.random() * arr.length))
     return arr[index]
 }
