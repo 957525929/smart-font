ea78a85b07c7a5215fa66873ca725e2da613015a
Linh <307148762@qq.com>
2021-08-11 17:56:55 +0800

    01 智能锁web 智能锁维修配置页面修改

--- 1-lock/src/views/dashboard/data/repairData.js
+++ 1-lock/src/views/dashboard/data/repairData.js
@@ -4,6 +4,7 @@ export const repairData = [
         name: '李司',
         phone: '13235678525',
         type: '智能锁厂商',
+        thingtype: '故障',
         area: '中国烟草总公司福建省公司机关A区域'
     },
     {
@@ -11,6 +12,15 @@ export const repairData = [
         name: '赵鎏',
         phone: '17658612545',
         type: '后勤管理',
+        thingtype: '失联',
         area: '中国烟草总公司福建省公司机关B区域'
-    }
+    },
+    {
+        key: 3,
+        name: '王武',
+        phone: '13256457852',
+        type: '后勤管理',
+        thingtype: '低电量',
+        area: '中国烟草总公司福建省公司机关A区域'
+    },
 ]
