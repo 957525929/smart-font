9a9f8b9cd7de3dc6169619b1fe232cd2c65c97cf
Linh <307148762@qq.com>
2021-08-27 18:02:13 +0800

    02 餐卡充值

--- 2-rechargeH5/pages/recharge/pay.vue
+++ 2-rechargeH5/pages/recharge/pay.vue
@@ -4,6 +4,43 @@
 			<block slot="backText">返回</block>
 			<block slot="content">支付</block>
 		</cu-custom>
+
+		<view class="cu-list menu sm-border card-menu margin-top">
+			<view class="cu-form-group">
+				<view class="title">姓名</view>
+				<view>{{ cardData.name }}</view>
+			</view>
+			
+			<view class="cu-form-group">
+				<view class="title">餐卡号</view>
+				<view>{{ cardData.cardID }}</view>
+			</view>
+			
+			<view class="cu-form-group">
+				<view class="title">充值金额</view>
+				<view>{{ payData }} 元</view>
+			</view>
+			
+			<view class="cu-form-group">
+				<view class="title">支付方式</view>
+				<view>{{ payMethod }}</view>
+			</view>
+			
+			<radio-group class="block" @change="RadioChange">
+				<view class="cu-form-group">
+					<view class="title">微信支付</view>
+					<radio :class="radio=='A'?'checked':''" :checked="radio=='A'?true:false" value="A"></radio>
+				</view>
+				<view class="cu-form-group">
+					<view class="title">支付宝支付</view>
+					<radio :class="radio=='B'?'checked':''" :checked="radio=='B'?true:false" value="B"></radio>
+				</view>
+			</radio-group>
+		</view>
+		
+		<view class="padding margin flex">
+			<button class="cu-btn bg-blue lg" style="flex: 1;" @tap="pay">确认支付</button>
+		</view>
 	</view>
 </template>
 
@@ -11,15 +48,32 @@
 	export default {
 		onLoad(item) {
 			this.payData = JSON.parse(item.data)
-			console.log(this.payData)
 		},
 		data() {
 			return {
+				cardData: {
+					name: '袁洪',
+					cardID: '6217 4545 0321 7821 123',
+					money: 500,
+					status: '正常',
+				},
 				payData: '',
+				radio: 'A',
+				payMethod: '微信支付',
 			}
 		},
 		methods: {
-			
+			RadioChange(e) {
+				this.radio = e.detail.value
+				this.payMethod = this.radio == 'A' ? '微信支付' : '支付宝支付'
+			},
+			pay(){
+				if(this.radio == 'A'){
+					console.log('微信支付')
+				}else{
+					console.log('支付宝支付')
+				}
+			}
 		}
 	}
 </script>
