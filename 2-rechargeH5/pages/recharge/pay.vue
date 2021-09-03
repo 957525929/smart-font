79b25ed0d8d6e6743ead68ccad2f722cf0ee5cdb
Linh <307148762@qq.com>
2021-09-03 14:38:56 +0800

    02 餐卡充值

--- 2-rechargeH5/pages/recharge/pay.vue
+++ 2-rechargeH5/pages/recharge/pay.vue
@@ -10,34 +10,40 @@
 				<view class="title">餐卡号</view>
 				<view>{{ cardData.cardID }}</view>
 			</view>
-			
+
 			<view class="cu-form-group">
 				<view class="title">姓名</view>
 				<view>{{ cardData.name }}</view>
 			</view>
-			
+
 			<view class="cu-form-group">
 				<view class="title">充值金额</view>
 				<view>{{ payData }} 元</view>
 			</view>
-			
+
 			<view class="cu-form-group">
 				<view class="title">支付方式</view>
 				<view>{{ payMethod }}</view>
 			</view>
-			
+
 			<radio-group class="block" @change="RadioChange">
 				<view class="cu-form-group">
-					<view class="title">微信支付</view>
+					<view class="title">
+						<text class="cuIcon-weixin text-green margin-right-xs"></text>
+						微信支付
+					</view>
 					<radio :class="radio=='A'?'checked':''" :checked="radio=='A'?true:false" value="A"></radio>
 				</view>
 				<view class="cu-form-group">
-					<view class="title">支付宝支付</view>
+					<view class="title">
+						<image class="margin-right-xs" src="../../static/zhifubao.png" style="width: 15px" mode="widthFix"></image>
+						支付宝支付
+					</view>
 					<radio :class="radio=='B'?'checked':''" :checked="radio=='B'?true:false" value="B"></radio>
 				</view>
 			</radio-group>
 		</view>
-		
+
 		<view class="padding margin flex">
 			<button class="cu-btn bg-blue lg" style="flex: 1;" @tap="pay">确认支付</button>
 		</view>
@@ -67,10 +73,10 @@
 				this.radio = e.detail.value
 				this.payMethod = this.radio == 'A' ? '微信支付' : '支付宝支付'
 			},
-			pay(){
-				if(this.radio == 'A'){
+			pay() {
+				if (this.radio == 'A') {
 					console.log('微信支付')
-				}else{
+				} else {
 					console.log('支付宝支付')
 				}
 			}
